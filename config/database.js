babfc323c2a12d8246d7b592c05ff9c5361694bf
Ted <metedace@gmail.com>
2025-06-25 08:42:39 +0400

    Update database configuration to enhance connection settings and enable debug mode

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-        client: 'postgres',
-        connection: {
-            connectionString: env('DATABASE_URL')
-        },
-        debug: true,
-        pool: { min: 0, max: 7 },
-    }
+      connectionString: env("DATABASE_URL"),
+    },
+    debug: env.bool("DATABASE_DEBUG", false),
+    pool: { min: 0, max: 7 },
+  },
 });
